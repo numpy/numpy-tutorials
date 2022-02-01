d88c1526149e7fcb29fdf9e720eb9fb8e894935d
Willi Hohenstein <willi.hohenstein@seburai.de>
2022-02-01 15:44:36 +0100

    moved random instance out of import statements

--- content/text_preprocessing.py
+++ content/text_preprocessing.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import argparse
 import numpy as np
-rng = np.random.default_rng() # crating the random instance
 import re # (https://docs.python.org/3/library/re.html) for tokenising textual data 
 import string # (https://docs.python.org/3/library/string.html) for string operations  
+
+# Crating the random instance
+rng = np.random.default_rng()
 
 class TextPreprocess:
     """Text Preprocessing for a Natural Language Processing model."""
